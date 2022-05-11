5f4e4ed56fcf35395dc236de3ecb93bdd7fd8ef8
skrrrlev <reimer.frickmann@gmail.com>
2022-05-11 13:42:59 +0200

    Added paths to library.

---
+++ MTLib/constants.py
@@ -0,0 +1,12 @@
+
+from enum import Enum
+
+class PATH(Enum):
+    ROOT = __file__[:(__file__[:__file__.rfind('/')]).rfind('/')+1]
+    FIGURES = ROOT + 'Figures/'
+    DATA = ROOT + 'Data/'
+    MAPS = DATA + 'Maps'
+    SCRIPTS = ROOT + 'Scripts/'
+
+if __name__ == '__main__':
+    print(PATHS.ROOT.value)
